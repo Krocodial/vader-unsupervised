98429600c7f6d9d5ec543d7b4d6d2ea3671d8491
krocodial <louis.kraak@gov.bc.ca>
2019-04-23 11:35:14 -0700

    ip mapping done, testing strat

--- encoding.py
+++ encoding.py
@@ -8,6 +8,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn.ensemble import IsolationForest
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import tkinter
@@ -16,17 +19,13 @@ from geoip import geolite2
 clean_df = log_to_dataframe.LogToDataFrame('bro/clean_traffic/http.log')
 mixed_df = log_to_dataframe.LogToDataFrame('bro/mixed_traffic/http.log')
 
-print(clean_df.head())
-print(mixed_df.head())
+#print(clean_df.head())
+#print(mixed_df.head())
 #trans_depth
 features = ['ts', 'day', 'id.resp_h', 'id.resp_p', 'method', 'host', 'user_agent', 'request_body_len', 'response_body_len', 'status_code', 'info_code']
-#features = ['id.orig_h', 'id.resp_h']
 
 clean_df = clean_df.reset_index()
 mixed_df = mixed_df.reset_index()
-
-#clean_df = clean_df[clean_df.service != 'dns']
-#mixed_df = mixed_df[mixed_df.service != 'dns']
 
 def convert(ip):
 	match = geolite2.lookup(ip)
@@ -63,10 +62,7 @@ mixed_df['label'] = 'score'
 print(clean_df.head())
 print(mixed_df.head())
 
-#clean_df = clean_df.reset_index(drop=True)
-#mixed_df = mixed_df.reset_index(drop=True)
-
-
+'''
 for col in clean_df:
 	clean_df['id.resp_p'] = clean_df['id.resp_p'].astype('category')
 	clean_df['id.resp_h'] = clean_df['id.resp_h'].astype('category')
@@ -74,15 +70,13 @@ for col in clean_df:
 for col in mixed_df:
 	mixed_df['id.resp_p'] = mixed_df['id.resp_p'].astype('category')
 	mixed_df['id.resp_h'] = mixed_df['id.resp_h'].astype('category')
-
-print(clean_df.dtypes)
+'''
 
 clean_df.to_csv('clean_output.csv')
 mixed_df.to_csv('mixed_output.csv')
 
 
 concat_df = pd.concat([clean_df, mixed_df])
-#features_df = pd.get_dummies(concat_df, columns=['id.resp_h', 'id.resp_p', 'proto', 'service', 'orig_bytes', 'resp_bytes'], dummy_na=True)
 
 features_df = pd.get_dummies(concat_df, columns=['id.resp_h', 'id.resp_p', 'method', 'host', 'user_agent', 'status_code', 'info_code'])
 features_df.to_csv('concatted_output.csv')
@@ -93,56 +87,62 @@ score_df = features_df[features_df['label'] == 'score']
 train_df = train_df.drop('label', axis=1)
 score_df = score_df.drop('label', axis=1)
 
-#bro_df[features].to_csv('output.csv')
-#train_df.to_csv('clean_output.csv')
-#score_df.to_csv('mixed_output.csv')
-
 clean_matrix = train_df.to_numpy()
 mixed_matrix = score_df.to_numpy()
-
-
 
 def isolationFor(clean_matrix, mixed_matrix, percents):
 	uniq_uris = []
 	for percent in percents:
-		model = IsolationForest(contamination=percent).fit(clean_matrix)
+		model = IsolationForest(n_estimators=1000, behaviour='new', max_samples=0.8, contamination=percent).fit(clean_matrix)
+		#model = IsolationForest(contamination=percent).fit(clean_matrix)
 		#results = mixed_df[features][model.predict(mixed_matrix) == -1]
 		results = orig_df[model.predict(mixed_matrix) == -1]
 		name = 'isolation_' + str(percent)
 		results.to_csv('output/' + name + '_outliers.csv')
 		parseResults(results, name)
 
-		#uris = []
-		#df = results['host']
-		#uris = df.values.tolist()
-		#uniq_uris = uniq_uris + uris
-		#print(list(set(uniq_uris)))
 
-def robustCovariance():
-	pass
+def robustCovariance(clean_matrix, mixed_matrix, percents):
+	for percent in percents:
+		model = EllipticEnvelope(contamination=percent).fit(clean_matrix)
+		results = orig_df[model.predict(mixed_matrix) == -1]
+		name = 'covariance_' + str(percent)
+		results.to_csv('output/' + name + '_outliers.csv')
+		parseResults(results, name)
 
 def oneClassSVM():
-	pass
+	for percent in percents:
+		model = EllipticEnvelope(contamination=percent).fit(clean_matrix)
+		results = orig_df[model.predict(mixed_matrix) == -1]
+		name = 'covariance_' + str(percent)
+		results.to_csv('output/' + name + '_outliers.csv')
+		parseResults(results, name)
 
 def localOutlierFactor():
 	pass
 	#https://scikit-learn.org/stable/modules/outlier_detection.html
 
 def parseResults(results, name):
-	df = results['host']
-	uris = df.values.tolist()
-	uniq = list(set(uris))	
-	writer =  csv.writer(open('output/' + name + '.csv', 'w+'))
-	vt = VT('2c90cbca5025f72aa9fcf5275ae91198c4929fd34a7dd5b80ff4c2aeaeef6f54')
-	for uri in uniq:
-		response = vt.get_url_report(uri)
-		if 'results' not in response:
-			print(response, uri)
-		print(response['results']['positives'])
-		writer.writerow([uri, response['results']['positives']])
-	
-percents = [.01, .05, .10]
-isolationFor(clean_matrix, mixed_matrix, percents)
+	mapping = {}
+	with open('ip_mapping.csv', newline='') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			mapping[row[0]] = row[1]
 
+	result_hosts = results['host'].tolist()
+	total = len(result_hosts)
+	mal = 0
+	for host in result_hosts:
+		if int(mapping[host]) != 0:
+			mal = mal + 1
+	print('-----' + name + ' results-----')
+	print(total)
+	print(mal)	
+	print('---end---')
+
+
+percents = [.0, .01, .05, .10, .20]
+isolationFor(clean_matrix, mixed_matrix, percents)
+robustCovariance(clean_matrix, mixed_matrix, percents)
 
 
